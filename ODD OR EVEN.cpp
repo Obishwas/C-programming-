5923ce4fe8ebc4eaaa611cf07ce221956c571452
bishwas <33860671+bshwas@users.noreply.github.com>
2018-02-01 17:55:42 +0545

    Update ODD OR EVEN.cpp

--- ODD OR EVEN.cpp
+++ ODD OR EVEN.cpp
@@ -1,15 +1,17 @@
+/*This program show whether the number is odd or even*/
+
 #include<stdio.h>
 int main()
 {
 	int num;
 	printf("Enter any number :");
 	scanf("%d",&num);
-	if(num%2==0)
+	if(num%2==0)/*anynumber divisible by 2 is always even*/
 	{ 
 	  printf("Given number is Even");
 	  
 	}
-    else if(num%2==1)
+    else if(num%2==1)/*anynumber not divisible by 2 is odd*/
 	{
 		printf("Given number is Odd");
 	}	
